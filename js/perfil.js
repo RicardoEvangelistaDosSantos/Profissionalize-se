b1c2a8a09a1b097ae396213fb3fd77601119ab55
gabriel H. <129757379+GabryelHenryque@users.noreply.github.com>
2024-11-29 15:30:07 -0300

    tela perfil 70%

--- js/perfil.js
+++ js/perfil.js
@@ -1,3 +1,5 @@
+
+//#region SELECTION CIDADE E ESTADO
 const id_estado = document.getElementById("id_estado");
 const id_cidade = document.getElementById("id_cidade");
 let cacheEstados = [];
@@ -46,10 +48,80 @@ function populateCidades(cidades) {
 }
 function selectCidade() {return id_cidade.value }
 
-
-
-
 console.log("valor cidade",id_cidade.value,"nome cidade",id_cidade.textContent)
 
+//#endregion
+
+//#region INPUT IMAGEM (BANNER E ICONE)
+
+/*banner*/
+const inputFile = document.querySelector("#file_banner");
+const sect = document.querySelector(".section-1");
+const sec_cut = document.querySelector(".section-cut");
+const cropperContainer = document.getElementById("cropper-container");
+const cropperImage = document.getElementById("cropper-image");
+const cropButton = document.getElementById("crop-button");
+const cancelButton = document.getElementById("cancel-button");
+
+let cropper = null;
+
+inputFile.addEventListener("change", function (e) {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      cropperImage.src = event.target.result; // Define a imagem no elemento <img>
+      sec_cut.style.display = "block";
+      cropperContainer.style.display = "block"; // Mostra o container do cropper
+      // Inicializa o cropper.js
+      cropper = new Cropper(cropperImage, {
+        aspectRatio: 16 / 9, // Proporção do recorte (opcional)
+        viewMode: 1,
+        autoCropArea: 0.8,
+      });
+        // Rolagem para a section-cut
+        sec_cut.scrollIntoView({
+            behavior: 'smooth', // Rolagem suave
+            block: 'center' // Alinha o item no centro da tela
+        });
+    };
 
 
+
+    reader.readAsDataURL(file);
+  }
+
+  // Reseta o valor do input para permitir seleção do mesmo arquivo
+  inputFile.value = "";
+});
+
+// Ação ao clicar no botão "Recortar"
+cropButton.addEventListener("click", function () {
+  if (cropper) {
+    const croppedImage = cropper.getCroppedCanvas().toDataURL("image/png"); // Obtém a imagem recortada
+    sect.style.backgroundImage = `url('${croppedImage}')`; // Define como fundo
+    cropper.destroy(); // Remove o cropper
+    cropper = null; // Reseta a instância do cropper
+    sec_cut.style.display = "none";
+    cropperContainer.style.display = "none"; // Esconde o container
+
+  }
+});
+
+// Ação ao clicar no botão "Cancelar"
+cancelButton.addEventListener("click", function () {
+  if (cropper) {
+    cropper.destroy(); // Remove o cropper
+    cropper = null; // Reseta a instância do cropper
+    sec_cut.style.display = "none";
+    cropperContainer.style.display = "none"; // Esconde o container
+  }
+});
+
+
+
+
+/*icone*/
+
+//#endregion
