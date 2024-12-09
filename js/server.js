19d99e6446eabaf813314de35e2df8098e9b8aa3
Ricardo Evangelista <164408961+RicardoEvangelistaDosSantos@users.noreply.github.com>
2024-12-09 20:31:07 -0300

    a

--- js/server.js
+++ js/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "",
+    user: "root",
     password: "root",
     database: "profissionalize_se",
 });
